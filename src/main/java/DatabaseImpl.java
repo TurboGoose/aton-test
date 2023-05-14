6f5a901860a3918078418d8a4654ecc4c73c9f3f
TurboGoose <antonpuckich@gmail.com>
2023-05-14 19:48:05 +0300

    Implement sorting by id

--- src/main/java/DatabaseImpl.java
+++ src/main/java/DatabaseImpl.java
@@ -1,53 +1,66 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 public class DatabaseImpl implements Database {
-    private final List<Account> data = new ArrayList<>();
+    private final List<Account> accounts = new ArrayList<>(); // sorted by id
 
     @Override
-    public void add(Account account) { // O(1)
-        data.add(account);
+    public void add(Account account) { // O(N)
+        accounts.add(findIndexToInsert(account.getId()), account);
+    }
+
+    private int findIndexToInsert(long id) {
+        int index = Collections.binarySearch(accounts, id, Comparator.comparingLong(
+                acc -> acc instanceof Account ? ((Account) acc).getId() : (long) acc
+        ));
+        if (index >= 0) {
+            throw new IllegalArgumentException();
+        }
+        return -index - 1;
     }
 
     @Override
     public void delete(long id) { // O(N)
-        data.removeIf(acc -> acc.getId() == id);
+        try {
+            accounts.remove(findIndexOfExistingAccount(id));
+        } catch (IllegalArgumentException ignore) {}
     }
 
     @Override
-    public void update(Account account) { // O(N)
-        Account accountToUpdate = null;
-        for (Account acc : data) {
-            if (acc.getId() == account.getId()) {
-                accountToUpdate = acc;
-                break;
-            }
-        }
-        if (accountToUpdate == null) {
-            return;
-        }
+    public void update(Account account) { // O(logN)
+        Account accountToUpdate = accounts.get(findIndexOfExistingAccount(account.getId()));
         accountToUpdate.setName(account.getName());
         accountToUpdate.setValue(account.getValue());
     }
 
+    private int findIndexOfExistingAccount(long id) {
+        int index = Collections.binarySearch(accounts, id, Comparator.comparingLong(
+                acc -> acc instanceof Account ? ((Account) acc).getId() : (long) acc
+        ));
+        if (index < 0) {
+            throw new IllegalArgumentException();
+        }
+        return index;
+    }
+
     @Override
-    public Optional<Account> getById(long id) { // O(N)
-        return data.stream()
-                .filter(acc -> acc.getId() == id)
-                .findFirst();
+    public Optional<Account> getById(long id) { // O(logN)
+        try {
+            return Optional.of(accounts.get(findIndexOfExistingAccount(id)));
+        } catch (IllegalArgumentException exc) {
+            return Optional.empty();
+        }
     }
 
     @Override
     public List<Account> getByName(String name) { // O(N)
-        return data.stream()
+        return accounts.stream()
                 .filter(acc -> acc.getName().equals(name))
                 .toList();
     }
 
     @Override
     public List<Account> getByValue(double value) { // O(N)
-        return data.stream()
+        return accounts.stream()
                 .filter(acc -> acc.getValue() == value)
                 .toList();
     }
--- src/test/java/ListDatabaseTest.java
+++ src/test/java/ListDatabaseTest.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DatabaseTest {
     Database db;
@@ -16,10 +17,18 @@ class DatabaseTest {
     }
 
     @Test
-    public void whenAddThenAdd() {
+    public void whenAddAccountWithNonExistentIdThenAdd() {
         Account account = new Account(1, "Alex", 50.1);
         db.add(account);
         assertThat(db.getById(1).get(), is(account));
+    }
+
+    @Test
+    public void whenAddAccountWithExistentIdThenThrow() {
+        Account account1 = new Account(1, "Alex", 50.1);
+        Account account2 = new Account(1, "Anton", 60.1);
+        db.add(account1);
+        assertThrows(IllegalArgumentException.class, () -> db.add(account2));
     }
 
     @Test
@@ -32,13 +41,9 @@ class DatabaseTest {
     }
 
     @Test
-    public void whenUpdateByNonExistentIdThenNothing() {
-        Account account = new Account(1, "Alex", 50.1);
-        db.add(account);
-        Account updated = new Account(2, "Alexander", 100);
-        db.update(updated);
-        assertThat(db.getById(1).get(), is(account));
-        assertThat(db.getById(2).isEmpty(), is(true));
+    public void whenUpdateByNonExistentIdThenThrow() {
+        Account updated = new Account(1, "Alex", 50.1);
+        assertThrows(IllegalArgumentException.class, () -> db.update(updated));
     }
 
     @Test
