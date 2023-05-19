1140f47a3e057958b43de0621eda6f4e2532e91c
TurboGoose <antonpuckich@gmail.com>
2023-05-19 19:53:24 +0300

    Add lombok and apply it for Account model

--- src/main/java/models/Account.java
+++ src/main/java/models/Account.java
@@ -1,57 +1,12 @@
 package models;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Account {
     private final long id; // primary key, sorted -> binary search
     private String name; // radix tree
-    private double value; // some tree (t-tree?)
-
-    public Account(long id, String name, double value) {
-        this.id = id;
-        this.name = name;
-        this.value = value;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getValue() {
-        return value;
-    }
-
-    public void setValue(double value) {
-        this.value = value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Account account = (Account) o;
-        return id == account.id && Double.compare(account.value, value) == 0 && Objects.equals(name, account.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, value);
-    }
-
-    @Override
-    public String toString() {
-        return "models.Account{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", value=" + value +
-                '}';
-    }
+    private double value; // rb tree (TreeMap)
 }
